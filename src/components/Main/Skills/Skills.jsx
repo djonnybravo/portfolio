410a47a6be6275c4c8c4db4702c67877786be05c
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-05-13 22:11:41 +0300

    add structure for Skills.jsx

--- src/components/Main/Skills/Skills.jsx
+++ src/components/Main/Skills/Skills.jsx
@@ -2,8 +2,40 @@ import React from 'react';
 import styles from './Skiils.module.css'
 const Skills = () => {
     return (
-        <div>
-            skills
+        <div className={styles.containter}>
+            <div className={styles.skills}>
+                <div className={styles.skills_Item}>
+                    <h3>React</h3>
+                    <img src="" alt="icon"/>
+                    <p>some text</p>
+                </div>
+                <div className={styles.skills_Item}>
+                    <h3>React</h3>
+                    <img src="" alt="icon"/>
+                    <p>some text</p>
+                </div>
+                <div className={styles.skills_Item}>
+                    <h3>React</h3>
+                    <img src="" alt="icon"/>
+                    <p>some text</p>
+                </div>
+                <div className={styles.skills_Item}>
+                    <h3>React</h3>
+                    <img src="" alt="icon"/>
+                    <p>some text</p>
+                </div>
+                <div className={styles.skills_Item}>
+                    <h3>React</h3>
+                    <img src="" alt="icon"/>
+                    <p>some text</p>
+                </div>
+                <div className={styles.skills_Item}>
+                    <h3>React</h3>
+                    <img src="" alt="icon"/>
+                    <p>some text</p>
+                </div>
+
+            </div>
         </div>
     );
 };

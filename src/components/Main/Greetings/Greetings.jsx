f35a01dfa931f854a9328544165ab6b1ef7d33a3
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-05-13 21:55:06 +0300

    add structure for Greetings.jsx

--- src/components/Main/Greetings/Greetings.jsx
+++ src/components/Main/Greetings/Greetings.jsx
@@ -3,8 +3,15 @@ import styles from './Greetings.module.css'
 
 const Greetings = () => {
     return (
-        <div>
-
+        <div className={styles.container}>
+            <div className={styles.hello}>
+                <h1>Hello World</h1>
+                <h2>Evgenis Solovev</h2>
+                <p>Some text</p>
+            </div>
+            <div className={styles.img}>
+                <img src="" alt="photo"/>
+            </div>
         </div>
     );
 };

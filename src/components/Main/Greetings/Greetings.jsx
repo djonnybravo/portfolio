1282dfee12cc7f7b3421b8dc6a3de6302840868e
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-05-13 21:41:17 +0300

    add Greetings.jsx

---
+++ src/components/Main/Greetings/Greetings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Greetings.module.css'
+
+const Greetings = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Greetings;

f330bfe9d9f9cbf23ebb3f90f2bb965eb846804a
djonnybravo <djonnybravo29@gmail.com>
2023-05-22 16:55:58 +0300

    feat: add old project components

---
+++ components/Main/MainContent.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Skills from "./Skills/Skills";
+import Projects from "./Projects/Projects";
+import ContactForm from "./ContactForm/ContactForm";
+import Greetings from "./Greetings/Greetings";
+import Wishes from "./Wishes/Wishes";
+
+const MainContent = () => {
+    return (
+        <main>
+            <Greetings/>
+            <Skills/>
+            <Projects/>
+            <Wishes/>
+            <ContactForm/>
+
+        </main>
+    );
+};
+
+export default MainContent;
---
+++ components/Main/Wishes/Wishes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Wishes.module.css'
+
+const Wishes = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Wishes;

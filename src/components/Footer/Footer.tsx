2ac91d00d5de9aef14c024a40ff01ed6650141a4
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-06-04 17:22:29 +0300

    feat: add basic footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import styles from './Footer.module.css'
 
 const Footer = () => {
+
+    const links = [
+        {link: '', src: "", title:"telegram"},
+        {link: '', src: "", title:"github"},
+        {link: '', src: "", title:"linkedin"},
+    ]
+
     return (
         <footer>
-            footer
+            <div className={styles.container}>
+
+                {links.map((item) => {
+                    return (
+                        <a href={item.link}>
+                            <img src={item.src} alt={item.title}/>
+                        </a>
+                    )
+                })}
+
+            </div>
         </footer>
     );
 };

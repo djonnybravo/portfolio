efb3e24c81c6ac8c97631bba5c6082ed3a32cb8e
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-06-04 19:10:33 +0300

    fix

--- src/components/Main/ContactForm/ContactForm.tsx
+++ src/components/Main/ContactForm/ContactForm.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-
+import styles from './ContactForm.module.css'
 const ContactForm = () => {
 
     return (
-        <form >
-            <fieldset>
-                <label htmlFor="">your name: <input type="text"/></label>
-                <label htmlFor="">your email: <input type="text"/></label>
-                <label htmlFor="">message:<textarea name="" id="" ></textarea></label>
-            </fieldset>
-        </form>
+        // <div className={styles.container}>
+        //     <div className={styles.contacts}>
+        //         <h3>Контакты</h3>
+        //         <span>телефон</span>
+        //         <span>телеграм</span>
+        //     </div>
+            <form className={styles.form}  action={'/'} method="post">
+                <fieldset>
+                    <legend>Напишите мне</legend>
+                    <label >your name: <input type="text"/></label>
+                    <label >your email: <input type="text"/></label>
+                    <label>Text</label>
+                    <label ><textarea name="" id="" ></textarea></label>
+                </fieldset>
+            </form>
+
+        // </div>
+
     );
 };
 

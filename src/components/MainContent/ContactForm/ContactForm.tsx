7918874b3ae3a9a3c2e2b82e6110e879010092cf
djonnybravo <djonnybravo29@gmail.com>
2023-06-05 19:15:39 +0300

    feat: add contact form

--- src/components/MainContent/ContactForm/ContactForm.tsx
+++ src/components/MainContent/ContactForm/ContactForm.tsx
@@ -2,70 +2,34 @@ import React from 'react';
 import styles from './ContactForm.module.css'
 
 const ContactForm = () => {
+
     return (
-        <section>
-            <div className={styles.container}>
+        <div className={styles.feedbackContainer}>
+            <section id={"feedback"}>
+                <h1>Форма обратной связи</h1>
+                <form>
 
-                <div className={styles.content}>
-
-                    <div className={styles.leftSide}>
-                        <div className="address details">
-
-                            <i className="fas fa-map-marker-alt"></i>
-
-                            <div className="topic">Адрес</div>
-                            <div className="text-one">г. Москва</div>
-                            <div className="text-two">Пресненская наб., 8 стр. 1</div>
-                        </div>
-                        <div className="phone details">
-                            <i className="fas fa-phone-alt"></i>
-                            <div className="topic">Телефон</div>
-                            <div className="text-one">8-800-000-00-00</div>
-                            <div className="text-two">8-900-000-00-00</div>
-                        </div>
-                        <div className={styles.details}>
-                            <i className="fas fa-envelope"></i>
-                            <div className="topic">Email</div>
-                            <div className="text-one">support@site.com</div>
-                            <div className="text-two">admin@site.com</div>
-                        </div>
+                    <div className={styles.field}>
+                        <input type={"text"} id={"name"} placeholder={"Как Вас зовут?"}/>
+                        <label htmlFor={"name"}>Имя</label>
                     </div>
 
-
-                    <div className={styles.rightSide}>
-
-
-                        <div className={styles.topicText}>Отправьте нам сообщение</div>
-
-
-                        <p>
-                            Если у вас есть какие-то вопросы или предложения по сотрудничеству -
-                            заполните форму ниже
-                        </p>
-
-                        <form action="#">
-
-                            <div className={styles.inputBox}>
-                                <input type={"text"} placeholder={"Ваше имя"}/>
-                            </div>
-                            <div className={styles.inputBox}>
-                                <input type={"text"} placeholder={"Введите email"}/>
-                            </div>
-                            <div className={styles.inputBox}>
-                                <input type={'text'} placeholder={"Введите телефон"}/>
-                            </div>
-                            <div className={styles.inputBox + " " + styles.messageBox}>
-                                <textarea placeholder={'Сообщение'}></textarea>
-                            </div>
-                            <div className={styles.button}>
-                                <input type={'button'} value={'Отправить'}/>
-                            </div>
-                        </form>
+                    <div className={styles.field}>
+                        <input type={"text"} id={"email"} placeholder={"Ваш e-mail адрес"}/>
+                        <label htmlFor={"email"}>E-mail</label>
                     </div>
-                </div>
-            </div>
 
-        </section>
+                    <div className={styles.field}>
+                        <textarea id="msg" rows={4} placeholder={"Ваше сообщение"}/>
+                        <label htmlFor="msg">Текст</label>
+                    </div>
+
+                    <input className="button" type="submit" value="Отправить"/>
+
+                </form>
+            </section>
+        </div>
+
     );
 };
 
